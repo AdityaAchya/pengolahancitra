1f844fbc4be23f1873cd3b2444a8caaa8e5e779a
AdityaAchya <123864099+AdityaAchya@users.noreply.github.com>
2024-05-12 00:13:51 +0700

    Add files via upload

--- home.py
+++ home.py
@@ -1,13 +1,9 @@
 import streamlit as st
 
 st.title('Profile')
-st.subheader('Nama : Adam Dwi Maulana')
-st.subheader('NIM : 312210242')
+st.subheader('Nama : Aditya Achya Ananta Nur')
+st.subheader('NIM : 312210714')
 st.subheader('Kelas : TI.22.B1')
 st.subheader('Prodi : Teknik Infomatika')
 
-st.image("./img/adam3.jpg", caption="Adam", use_column_width="Always", width=200)
-
-
-st.link_button("Instagram", "https://www.instagram.com/adam_webdev/")
-st.link_button("Github", "https://github.com/adam-webdev")
+st.image("./img/adit.jpg", caption="Adit", use_column_width="Always", width=200)
